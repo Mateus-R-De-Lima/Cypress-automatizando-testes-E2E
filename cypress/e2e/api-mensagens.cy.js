0021d99dbefdd527c45a0a951e7f0bc6e3410c9f
Mateus Rodrigues De Lima <70700338+limamateus@users.noreply.github.com>
2024-10-07 20:38:31 -0300

    FEAT - Protegendo dados sensíveis
    Exemplo de Dados sensíveis

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -4,13 +4,12 @@ describe('Api Adopet', {
         openMode: 1,
     },
 }, () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcwOTA0MTMwNSwiZXhwIjoxNzA5MzAwNTA1fQ.nhiaUyKdvN8RVxCkvne2gyI5n_pYnY_OMMdogMMqOlc`
     const tempoEsperado = Math.random() * 1000
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: { authorization }
+            headers: Cypress.env("authorization")
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -3,7 +3,7 @@
 
 describe('Página de cadastro', () => {
   beforeEach(() => {
-    cy.visit('https://adopet-frontend-iota.vercel.app/');
+    cy.visit(Cypress.env("url"));
     cy.get('[data-test="register-button"]').click();
   })
   it('Deve preencher os campos do cadastro corretamente e clicar no botão cadastrar', () => {
